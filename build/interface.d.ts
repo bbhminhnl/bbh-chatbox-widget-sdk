2224d0589b6b6e2e5d7e0cc9f4a6dade9fed9e2f
Andrew Nguyen <nguyenleminhdev@gmail.com>
2023-05-11 11:44:19 +0700

    update

--- build/interface.d.ts
+++ build/interface.d.ts
@@ -11,10 +11,47 @@ export interface CustomerInfo {
         fb_client_id: string;
         page_name?: string;
         client_name?: string;
+        token_partner: string;
+        current_staff_id: string;
+        current_staff_name: string;
+        conversation_id: string;
+        page_id: string;
+        conversation_index: number;
     };
     conversation_contact?: {
         client_phone?: string;
         client_email?: string;
+    };
+    conversation_message?: {
+        last_read_message: string;
+        last_message_time: number;
+        last_message: string;
+        last_message_type: string;
+        platform_type: string;
+    };
+    conversation_label?: {
+        label_id: string[];
+        snap_label: {
+            type: string;
+            text_color: string;
+            bg_color: string;
+            _id: string;
+            title: string;
+        }[];
+    };
+    conversation_last_note?: {};
+    conversation_staff?: {
+        fb_staff_id: string;
+        snap_staff: {
+            type: string;
+            is_auto_assign: boolean;
+            _id: string;
+            name: string;
+        };
+    };
+    conversation_chatbot?: {
+        bbh_public_token: string;
+        bbh_private_token: string;
     };
 }
 export interface GetCustomerInfoCallback {
